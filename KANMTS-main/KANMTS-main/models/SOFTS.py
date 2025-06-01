f43daae9c8fc03bd980ea1509ccff9f8a2570ce9
qq234567890lyj <1513024859@qq.com>
2025-06-01 11:41:57 +0800

    Update SOFTS.py

--- KANMTS-main/KANMTS-main/models/SOFTS.py
+++ KANMTS-main/KANMTS-main/models/SOFTS.py
@@ -18,37 +18,37 @@ class STAR(nn.Module):
         self.gen3 = nn.Linear(d_series + d_core, d_series)
         self.gen4 = nn.Linear(d_series, d_series)
 
-    # input（32,11,512）
+   
     def forward(self, input, *args, **kwargs):
         batch_size, channels, d_series = input.shape
 
 
-        # （32,11,512）
+      
         combined_mean = F.gelu(self.gen1(input))
         combined_mean = self.gen2(combined_mean)  # （32,11,512）
 
         # stochastic pooling
 
         if self.training:
-            # #（32,11,512）对 combined_mean
-            ratio = F.softmax(combined_mean, dim=1)  # softmax
-            ratio = ratio.permute(0, 2, 1)  # （32,512,11）
-            ratio = ratio.reshape(-1, channels)  # （32*512=16384,11）
-            indices = torch.multinomial(ratio, 1)  # 16384,1
-            indices = indices.view(batch_size, -1, 1).permute(0, 2, 1)  # （32,1,512）
-            combined_mean = torch.gather(combined_mean, 1, indices)  # （32,1,512）
-            combined_mean = combined_mean.repeat(1, channels, 1)  # （32,11,512）
+          
+            ratio = F.softmax(combined_mean, dim=1) 
+            ratio = ratio.permute(0, 2, 1) 
+            ratio = ratio.reshape(-1, channels) 
+            indices = torch.multinomial(ratio, 1) 
+            indices = indices.view(batch_size, -1, 1).permute(0, 2, 1) 
+            combined_mean = torch.gather(combined_mean, 1, indices) 
+            combined_mean = combined_mean.repeat(1, channels, 1)  
         else:
             weight = F.softmax(combined_mean, dim=1)
             combined_mean = torch.sum(combined_mean * weight, dim=1, keepdim=True).repeat(1, channels, 1)
 
         # mlp fusionMLP
 
-        combined_mean_cat = torch.cat([input, combined_mean], -1)  # （32,11,1024）
+        combined_mean_cat = torch.cat([input, combined_mean], -1) 
 
-        combined_mean_cat = F.gelu(self.gen3(combined_mean_cat)) # （32,11,512）
+        combined_mean_cat = F.gelu(self.gen3(combined_mean_cat)) 
         combined_mean_cat = self.gen4(combined_mean_cat)
-        output = combined_mean_cat  #（32,11,512）
+        output = combined_mean_cat  
 
         return output, None
 
@@ -79,7 +79,7 @@ class Model(nn.Module):
         # Decoder
         self.projection = nn.Linear(configs.d_model, configs.pred_len, bias=True)
 
-    # x_enc（32,96,7 ），x_mark_enc（32,96,4 ）
+  
     def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         # Normalization from Non-stationary Transformer
         if self.use_norm:
@@ -90,11 +90,11 @@ class Model(nn.Module):
 
         _, _, N = x_enc.shape
 
-        # （32,11,512）值嵌入[Batch Variate Time]
+       
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
-        enc_out, attns = self.encoder(enc_out, attn_mask=None) # enc_out（32,11,512）
+        enc_out, attns = self.encoder(enc_out, attn_mask=None) 
 
-        dec_out = self.projection(enc_out).permute(0, 2, 1)[:, :, :N]  # （32,96,7）
+        dec_out = self.projection(enc_out).permute(0, 2, 1)[:, :, :N]  
 
         # De-Normalization from Non-stationary Transformer
         if self.use_norm:
